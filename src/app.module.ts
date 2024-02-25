8b86cf6a430e34c54d47a23d58ae0de9af6c80d1
Arthur Lopes <arthurlopr12@gmail.com>
2024-02-25 14:30:34 -0300

    feat: add connection with db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './presentation/main/modules/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ['.env.developemnt.local'],
+      envFilePath: ['.env'],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: String(process.env.DB_PASSWORD),
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      type: 'postgres',
     }),
     UserModule,
   ],
